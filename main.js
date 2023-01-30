e508886344bf0e46af6503f0c4673dc4e579c471
Aleph Perth <arcanforce00@gmail.com>
2023-01-30 17:28:25 -0300

    Carrito de compras toggle

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
-const mailMenu = document.querySelector('.navbar-email')
-const desktopMenu = document.querySelector('.desktop-menu')
-const hambMenu = document.querySelector('img')
+const mailMenu = document.querySelector('.navbar-email') // Donde clickeo
+const desktopMenu = document.querySelector('.desktop-menu') // Ventana que aparece
+
+const hambMenu = document.querySelector('.hamburguer')
 const mobileMenu = document.querySelector('.mobile-menu')
 
+const cart = document.querySelector('.cart')
+const productDetail = document.querySelector('.product-detail')
+
+cart.addEventListener('click', toggleShoppingCart)
 mailMenu.addEventListener('click', toggleDesktopMenu)
 hambMenu.addEventListener('click', toggleMobileMenu)
 
@@ -12,4 +17,8 @@ function toggleDesktopMenu() {
 
 function toggleMobileMenu() {
     mobileMenu.classList.toggle('inactive')
+}
+
+function toggleShoppingCart() {
+    productDetail.classList.toggle('inactive')
 }
