217ce1a028ed84c96214ee7cbc7ef4358f38b81a
Aleph Perth <arcanforce00@gmail.com>
2023-01-30 16:32:15 -0300

    Fusión del menú en mobile

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const mailMenu = document.querySelector('.navbar-email')
 const desktopMenu = document.querySelector('.desktop-menu')
+const hambMenu = document.querySelector('img')
+const mobileMenu = document.querySelector('.mobile-menu')
 
 mailMenu.addEventListener('click', toggleDesktopMenu)
+hambMenu.addEventListener('click', toggleMobileMenu)
 
 function toggleDesktopMenu() {
         desktopMenu.classList.toggle('inactive')
+}
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive')
 }
