b0b4057f6128ae2047cbb427a6fbfce328827111
Aleph Perth <arcanforce00@gmail.com>
2023-02-01 21:19:39 -0300

    Carrito de compras

--- main.js
+++ main.js
@@ -1,15 +1,64 @@
-const mailMenu = document.querySelector('.navbar-email')
-const desktopMenu = document.querySelector('.desktop-menu')
-const hambMenu = document.querySelector('img')
-const mobileMenu = document.querySelector('.mobile-menu')
+const mailMenu = document.querySelector('.navbar-email'); // Donde hago click
+const desktopMenu = document.querySelector('.desktop-menu'); //Que se despliega
+const hambMenu = document.querySelector('.hamburger');
+const mobileMenu = document.querySelector('.mobile-menu');
+const cartMenu = document.querySelector('.cart');
+const shopCartMenu = document.querySelector('.shopping-cart-detail');
 
-mailMenu.addEventListener('click', toggleDesktopMenu)
-hambMenu.addEventListener('click', toggleMobileMenu)
+mailMenu.addEventListener('click', toggleDesktopMenu);
+hambMenu.addEventListener('click', toggleMobileMenu);
+cartMenu.addEventListener('click', toggleShopCartMenu);
 
 function toggleDesktopMenu() {
-        desktopMenu.classList.toggle('inactive')
+    const isShopCartMenuClosed = shopCartMenu.classList.contains('inactive')
+
+    if(!isShopCartMenuClosed) {
+        shopCartMenu.classList.add('inactive');
+    }
+
+    desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu() {
-    mobileMenu.classList.toggle('inactive')
+    const isShopCartMenuClosed = shopCartMenu.classList.contains('inactive')
+
+    if(!isShopCartMenuClosed) {
+        shopCartMenu.classList.add('inactive');
+    }
+
+    mobileMenu.classList.toggle('inactive');
 }
+
+function toggleShopCartMenu() {
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+
+    if(!isMobileMenuClosed) {
+        mobileMenu.classList.add('inactive');
+    } else if (!isDesktopMenuClosed) {
+        desktopMenu.classList.add('inactive');
+    }
+
+    shopCartMenu.classList.toggle('inactive');
+}
+
+let templateTest = null;
+
+templateTest =
+
+`
+<div class="product-card">
+    <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="Bike">
+    <div class="product-info">
+    <div>
+        <p>$120,00</p>
+        <p>Bike</p>
+    </div>
+    <figure>
+        <img src="./icons/bt_add_to_cart.svg" alt="">
+    </figure>
+    </div>
+</div>
+`
+
+document.querySelector('.cards-container').innerHTML = templateTest
